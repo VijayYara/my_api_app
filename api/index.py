84642830b03c2dbb0d2cbfa5f53c7490cbcf2074
VijayYara <21f3003091@ds.study.iitm.ac.in>
2025-01-25 23:32:37 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,3 +9,22 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+marks_data = [{
+        "name": "KXV",
+        "marks": 27
+    },
+    {
+        "name": "6LmFa",
+        "marks": 25
+    }]
+
+@app.route("/api", methods=["GET"])
+def get_marks():
+    # Get the 'name' parameters from the query string
+    names = request.args.getlist('name')
+    
+    # Fetch the marks for each name from the marks_data dictionary
+    marks = [i['marks'] for i in marks_data if i['name'] in names]
+    
+    return jsonify({"marks": marks})
